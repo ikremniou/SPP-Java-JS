3c66d2ad53d04b35b6e0dbf405dd556723525d03
Ilya Kremniou <Lolik21@users.noreply.github.com>
2018-02-03 12:42:57 +0300

    Hello web
     spring successfully configurated

---
+++ src/main/java/com/ilyashutko/buspark/HomeController.java
@@ -0,0 +1,16 @@
+package com.ilyashutko.buspark;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/hello")
+public class HomeController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("message","Hello spring");
+        return "hello";
+    }
+}
