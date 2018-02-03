25cb7bf1bd84d7762ce9a0a228f36aee04822e49
Ilya Kremniou <Lolik21@users.noreply.github.com>
2018-02-03 12:20:13 +0300

    Project created v0.01

---
+++ src/main/java/com/ilyashutko/buspark/BusparkApplication.java
@@ -0,0 +1,12 @@
+package com.ilyashutko.buspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusparkApplication.class, args);
+	}
+}
