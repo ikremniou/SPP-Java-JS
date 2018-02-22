676e71c8d74554e892f8d5e9d3888f7030d407a0
KostayShutko <shoker519@mail.ru>
2018-02-22 23:33:37 +0300

    Contact,About,login,register

--- src/main/java/com/ilyashutko/buspark/controller/HomeController.java
+++ src/main/java/com/ilyashutko/buspark/controller/HomeController.java
@@ -1,9 +1,15 @@
 package com.ilyashutko.buspark.controller;
 
+import com.ilyashutko.buspark.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -21,5 +27,30 @@ public class HomeController {
     @RequestMapping("/about")
     public String aboutPrint(ModelMap modelMap){
         return "about";
+    }
+
+    @RequestMapping("/login")
+    public String loginPrint(ModelMap modelMap){
+        return "login";
+    }
+
+    @PostMapping("/loginUser")
+    public String loginUserSubmit(@ModelAttribute User user) {
+        return "/";
+    }
+
+    @RequestMapping("/register")
+    public String registerPrint(ModelMap modelMap){
+        return "register";
+    }
+
+    @PostMapping("/registerUser")
+    public String registerUserSubmit(@ModelAttribute User user) {
+        return "/";
+    }
+
+    @RequestMapping("/tickets")
+    public String ticketsPrint(ModelMap model){
+        return "tickets";
     }
 }
